88ba0656547a0ed426dae5599957eac10a248a9a
AlcorBBX <lev.matisov55@gmail.com>
2022-10-29 06:35:47 +0500

    Update:  Post Controller  Post Service

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Types } from 'mongoose';
+import { Auth } from 'src/auth/jwt/auth.decorator';
+import { IdValidationPipe } from 'src/pipes/id.validation.pipe';
+import { CurrentUser } from 'src/user/decorators/user.decorator';
+import { postDto } from './post.dto';
 import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
-  constructor(private readonly postService: PostService) {}
+  constructor(private readonly postService: PostService) { }
+
+  @Get()
+  async getAll() {
+    return this.postService.getAll()
+  }
+
+  @Get('by-userId/:userId')
+  async getByUserId(@Param('userId', IdValidationPipe) userId: Types.ObjectId) {
+    return this.postService.byUserId(userId)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post()
+  @Auth()
+  async createPost(@CurrentUser('_id') userId: Types.ObjectId, @Body() dto: postDto) {
+    return this.postService.create(userId, dto)
+  }
+
+  @HttpCode(200)
+  @Delete(':id')
+  @Auth()
+  async deletePost(@Param('id', IdValidationPipe) id: Types.ObjectId) {
+    return this.postService.delete(id)
+  }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { Types } from 'mongoose';
+import { InjectModel } from 'nestjs-typegoose';
+import { postDto } from './post.dto';
+import { PostModel } from './post.model';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(
+        @InjectModel(PostModel)
+        private readonly postModel: ModelType<PostModel>
+    ) { }
+
+    async getAll() {
+        return this.postModel.find()
+            .select('-__v')
+            .populate('user', 'avatarPath name isVerified')
+            .exec()
+    }
+
+    async byUserId(userId: Types.ObjectId) {
+        return this.postModel.find({ user: userId })
+            .select('-__v')
+            .populate('user', 'avatarPath name isVerified')
+            .exec()
+    }
+
+    async create(userId: Types.ObjectId, { image, content }: postDto) {
+        return this.postModel.create({ image, content })
+    }
+
+    async delete(id: Types.ObjectId){
+        const deletePost = await this.postModel.findByIdAndDelete(id).exec()
+        if(!deletePost) throw new NotFoundException('Пост не найден')
+        return deletePost
+    }
+}
